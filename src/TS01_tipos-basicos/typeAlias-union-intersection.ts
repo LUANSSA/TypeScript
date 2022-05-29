3ac7b3626c6e0d957ecc02d77a67279aaa628d76
Luan Souza <alunoluansenai@gmail.com>
2022-05-29 19:02:05 -0300

    type Alias - union - intersection

--- src/TS01_tipos-basicos/typeAlias-union-intersection.ts
+++ src/TS01_tipos-basicos/typeAlias-union-intersection.ts
@@ -18,3 +18,52 @@ console.log(typeof luan);
 //object
 console.log(luan);
 //{ nome: 'Luan', lastName: 'Souza', birthDay: '23', age: 25 }
+
+
+/*
+Union type | pode ser considerado um || (ou)
+("a" || "b") de retorno de funções ou parâmetro
+
+exemplo
+function y(){
+    if(true){
+        return "a";
+    }
+    else{
+        return 23
+    }
+}
+
+*/
+type logLevel = "info" | "debug" | "error";
+
+function logMessage(message: string, level: logLevel){
+    return `${message} - ${level}`;
+}
+//no segundo parâmetro de logMessage() é obrigatório uma string "info" ou "debug" ou "error"
+console.log(logMessage("Deu tudo certo", "debug")); 
+//Deu tudo certo - debug
+
+//Intersection une vários typos diferentes
+type About = {
+    bio: string,
+    interesses: string[]
+};
+
+//Type Profile é a união de About e User
+type Profile  = About & User;
+
+const userWithProfile: Profile = {
+    nome: "Luan",
+    lastName: "Souza",
+    birthDay: "23",
+
+    bio: "Animado, empático, gentil",
+    interesses: ["Desenvolvimento de Sistemas", "Futebol", "Natação"]
+};
+
+
+type ComposeProfile = User & {
+    log: logLevel,
+    about: About;
+}
